5adaf1155091dd0e7e7e656e6b3cb84497244ad5
SydX-pages <xusyd1222@gmail.com>
2025-08-25 21:29:34 +0800

    first commit

---
+++ shades/scripts/launcher.sh
@@ -0,0 +1,7 @@
+#!/usr/bin/env bash
+
+#rofi -no-config -no-lazy-grab -show drun -modi drun -theme ~/.config/polybar/shades/scripts/rofi/launcher.rasi
+
+~/.config/rofi/launchers/type-5/launcher.sh
+
+
